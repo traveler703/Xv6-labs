994877b7037e476da8342f984c53b06d97950971
traveler703 <hongzhenren409@gmail.com>
2025-08-13 10:39:33 +0800

    speed up system call

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -81,6 +81,34 @@ int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 start_va;
+  int num_pages;
+  uint64 user_mask;
+
+  // 解析用户传入的参数
+  if (argaddr(0, &start_va) < 0)
+      return -1;
+  if (argint(1, &num_pages) < 0)
+      return -1;
+  if (argaddr(2, &user_mask) < 0)
+      return -1;
+
+  // 设置一个内核缓冲区来存储访问结果
+  uint64 mask = 0;
+  pagetable_t pagetable = myproc()->pagetable;
+
+  for (int i = 0; i < num_pages; i++) {
+    pte_t *pte = walk(pagetable, start_va + i * PGSIZE, 0);
+    if (pte && (*pte & PTE_V) && (*pte & PTE_A)) {
+      mask |= (1L << i);  // 设置对应位
+      *pte &= ~PTE_A;     // 清除访问位
+    }
+  }
+
+  // 将内核缓冲区内容拷贝到用户空间
+  if (copyout(pagetable, user_mask, (char *)&mask, sizeof(mask)) < 0)
+    return -1;
+
   return 0;
 }
 #endif
