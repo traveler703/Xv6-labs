37e3cc715a77bb39254407fa3164837308728df2
traveler703 <hongzhenren409@gmail.com>
2025-08-13 15:31:56 +0800

    print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,41 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Function to print a page table
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+
+  // Iterate over the top-level page table entries
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {  // Check if the entry is valid
+      uint64 child = PTE2PA(pte);
+      printf(" ..%d: pte %p pa %p\n", i, pte, child);
+      
+      if ((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+        // This entry points to a next-level page table
+        pagetable_t next_pagetable = (pagetable_t)child;
+        for (int j = 0; j < 512; j++) {
+          pte_t pte2 = next_pagetable[j];
+          if (pte2 & PTE_V) {
+            uint64 child2 = PTE2PA(pte2);
+            printf(" .. ..%d: pte %p pa %p\n", j, pte2, child2);
+
+            if ((pte2 & (PTE_R | PTE_W | PTE_X)) == 0) {
+              // This entry points to a next-level page table
+              pagetable_t next_pagetable2 = (pagetable_t)child2;
+              for (int k = 0; k < 512; k++) {
+                pte_t pte3 = next_pagetable2[k];
+                if (pte3 & PTE_V) {
+                  uint64 child3 = PTE2PA(pte3);
+                  printf(" .. .. ..%d: pte %p pa %p\n", k, pte3, child3);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
