2ef786a721882bd1ca8a79c4e7a2aecc93ba9cd7
traveler703 <hongzhenren409@gmail.com>
2025-08-17 22:24:56 +0800

    cow

--- kernel/vm.c
+++ kernel/vm.c
@@ -356,23 +356,22 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
     if(uncopied_cow(pagetable, va0)){
-      if(cow_alloc(pagetable, va0) != 0){
+      if(cow_alloc(pagetable, va0) != 0)
         return -1;  // 分配cow页失败
-      }
-      pa0 = walkaddr(pagetable, va0);
-      if(pa0 == 0)
-        return -1;
-      n = PGSIZE - (dstva - va0);
-      if(n > len)
-        n = len;
-      memmove((void *)(pa0 + (dstva - va0)), src, n);
-
-      len -= n;
-      src += n;
-      dstva = va0 + PGSIZE;
     }
-  return 0;
+    pa0 = walkaddr(pagetable, va0);
+    if(pa0 == 0)
+      return -1;
+    n = PGSIZE - (dstva - va0);
+    if(n > len)
+      n = len;
+    memmove((void *)(pa0 + (dstva - va0)), src, n);
+
+    len -= n;
+    src += n;
+    dstva = va0 + PGSIZE;
   }
+  return 0;
 }
 
 // Copy from user to kernel.
